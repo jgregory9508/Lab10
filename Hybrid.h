47a1436c00f2b6f53ee277ad750b90d8d2f52d73
jgregory9508 <jgregory9508@gmail.com>
2015-03-31 08:39:14 -0500

    commit

---
+++ Hybrid.h
@@ -0,0 +1,77 @@
+#if !defined (HYBRID_H)
+#define HYBRID_H
+
+#include "QueueLinked.h"
+using CSC2110::QueueLinked;
+#include "SortedListDoublyLinked.h"
+
+template < class T >
+class Hybrid
+{
+
+   private:
+      QueueLinked<T>* q;
+      SortedListDoublyLinked<T>* sldl;
+
+   public:
+      Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item));
+      ~Hybrid();
+
+      bool isEmpty();
+      void enqueue(T* item);
+      T* dequeue();
+      ListDoublyLinkedIterator<T>* iterator();
+
+};
+
+template < class T >
+Hybrid<T>::Hybrid(int (*comp_items) (T* item_1, T* item_2), int (*comp_keys) (String* key, T* item))
+{
+   q = new QueueLinked<T>();
+   sldl = new SortedListDoublyLinked<T>(comp_items, comp_keys);
+}
+
+template < class T >
+Hybrid<T>::~Hybrid()
+{
+   delete q;
+   delete sldl;
+}
+
+//DO THIS
+//complete the implementation for the Hybrid ADT in two different ways
+//as outlined in the Lab 10 description
+//simply comment the first implementation out when working on the second implementation
+//use the getKey method to dequeue/remove
+
+template < class T >
+bool Hybrid<T>::isEmpty()
+{
+	return q->isEmpty();
+}
+
+template < class T >
+T* Hybrid<T>::dequeue()
+{
+	DoubleNode<T>* node = q->dequeue();
+	T* item = sldl->remove(node);
+	return item;
+}
+
+template < class T >
+void Hybrid<T>::enqueue(T* item)
+{
+	DoubleNode<T>* node = sldl->addDN(item);
+	q->enqueue(node);
+}
+
+template < class T >
+ListDoublyLinkedIterator<T>* Hybrid<T>::iterator()
+{
+	return sldl->iterator();
+	
+}
+
+
+
+#endif
---
+++ Makefile
@@ -0,0 +1,25 @@
+AutomatedMakefile = am
+CC = g++
+
+FILES = HybridDriver.o
+EXECUTABLE = Hybrid.exe
+
+PROJECT_PATH = $(PROJECT_DIR)
+
+INC_DIRS = -I$(PROJECT_PATH)/CSC2110/ 
+LIB_DIRS = -L$(PROJECT_PATH)/CSC2110/ 
+LIBS = -lCSC2110 
+
+COMPILE = $(CC) $(INC_DIRS) -c
+LINK = $(CC) $(LIB_DIRS) -o
+
+all: Project
+
+Project: 		$(FILES)
+			$(LINK) $(EXECUTABLE) $(FILES) $(LIBS)
+
+HybridDriver.o:		Hybrid.h HybridDriver.cpp
+								$(COMPILE) HybridDriver.cpp
+
+
+
